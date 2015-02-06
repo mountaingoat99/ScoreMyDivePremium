44a6d9c5bcd089485af04feaae87aa58cf345039
Jeremey Rodriguez <jeremey.rodriguez@outlook.com>
2015-02-06 05:50:46 -0600

    Removed Cancel button. We will just use the back to home

--- app/src/main/java/com/rodriguez/scoremydivepremium/HowTo.java
+++ app/src/main/java/com/rodriguez/scoremydivepremium/HowTo.java
@@ -15,9 +15,7 @@ import android.widget.ImageButton;
 
 public class HowTo extends ActionBarActivity {
 
-    private Button cancel;
     private ImageButton link;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +26,6 @@ public class HowTo extends ActionBarActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-        cancel = (Button)findViewById(R.id.buttonOkay);
         link = (ImageButton)findViewById(R.id.imageButton);
 
         addListenerOnButton();
@@ -41,13 +38,6 @@ public class HowTo extends ActionBarActivity {
             public void onClick(View v) {
                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=jqpI7yi5DnY")));
                 Log.i("Video", "Video Playing App Tutorial");
-            }
-        });
-
-        cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
             }
         });
     }
